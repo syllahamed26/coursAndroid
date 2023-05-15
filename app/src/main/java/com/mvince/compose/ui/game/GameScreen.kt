cf4bf10ffed788ea783bf40307b98f84ecac5923
Hamed Cédric SYLLA <syllahamed26@yahoo.com>
2023-05-15 09:47:02 +0200

    continue questions

--- app/src/main/java/com/mvince/compose/ui/game/GameScreen.kt
+++ app/src/main/java/com/mvince/compose/ui/game/GameScreen.kt
@@ -1,13 +1,19 @@
 package com.mvince.compose.ui.game
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -16,10 +22,43 @@ fun GameScreen(){
     val viewModel = hiltViewModel<GameViewModel>()
 
     val questions =  viewModel.questions.collectAsState().value
+    val currentQuestion = viewModel.currentQuestion.collectAsState().value
+    val currentIndex = viewModel.currentIndex;
+
     Scaffold() {
-        Column(modifier = Modifier.padding(it)) {
-            questions.forEach {
-                Text(text = it.question)
+        Column(
+            modifier = Modifier.padding(it),
+            verticalArrangement = Arrangement.Center
+        ) {
+//            questions.forEach {
+//                Text(text = it.question)
+//            }
+            if (currentQuestion != null) {
+                Text(text = currentQuestion.question)
+                val answers = currentQuestion.incorrectAnswers + currentQuestion.correctAnswer
+                answers.shuffled().forEachIndexed { index, answer ->
+                    Row {
+                        RadioButton(
+                            selected = viewModel.currentAnswer == answer,
+                            onClick = {
+                                if(currentIndex + 1 <= questions.size){
+                                    viewModel.currentAnswer = answer
+                                    viewModel.validateAnswers(currentIndex)
+                                }
+                            }
+                        )
+                        ClickableText(
+                            text = AnnotatedString(answer),
+                            onClick = {
+                                if(currentIndex + 1 <= questions.size){
+                                    viewModel.currentAnswer = answer
+                                    viewModel.validateAnswers(currentIndex)
+                                }
+                            },
+                            modifier = Modifier.padding(12.dp)
+                        )
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/mvince/compose/ui/game/GameViewModel.kt
+++ app/src/main/java/com/mvince/compose/ui/game/GameViewModel.kt
@@ -1,5 +1,6 @@
 package com.mvince.compose.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mvince.compose.network.model.Result
@@ -11,6 +12,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,20 +24,51 @@ class GameViewModel @Inject constructor(
     val currentQuestion: StateFlow<Result?>
         get() = _currentQuestion
 
+    private val _currentScore = MutableStateFlow<Int>(0)
+    val currentScore: StateFlow<Int>
+        get() = _currentScore
+
     private val _questions = flow {
         val questions = questionRepository.getQuestions()
         emit(questions)
+        _currentQuestion.update {
+            questions.first()
+        }
     }.stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+
+    var currentIndex: Int = 0;
+    var currentAnswer: String? = null;
 
     val questions: StateFlow<List<Result>>
         get() = _questions
 
     fun validateAnswers(index: Int){
         //Manage score
+        if(currentAnswer == currentQuestion.value?.correctAnswer){
+            var points = 0;
+            points = currentQuestion.value?.difficulty?.let {
+                when(it){
+                    "easy" -> 2
+                    "medium" -> 5
+                    "hard" -> 10
+                    else -> 0
+                }
+            }!!
+            _currentScore.update {
+                it + points
+            }
+        }
+
+        //If last question, go to result screen
+        if(index == _questions.value.size - 1) {
+            Timber.tag("GameViewModel").d("Score: " + _currentScore.value)
+            return
+        }
 
         //Change question
         _currentQuestion.update {
-            _questions.value.get(index)
+            currentIndex = index + 1
+            _questions.value.get(currentIndex)
         }
     }
 }
